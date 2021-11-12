080093a8ee7ef2dfad5c2e9b912d20d0dd02baf8
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-11-12 12:58:36 +0000

    upload photo for list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,7 +16,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to root_path
     else
       render :new
     end
@@ -25,6 +25,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
