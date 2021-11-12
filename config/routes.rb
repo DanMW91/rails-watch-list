de5b424096763b8e2407d609b1b559266be2f8da
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-11-12 12:03:03 +0000

    pre-heroku commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:create]
+    resources :reviews, only: [:create]
   end
-    resources :bookmarks, only: [:destroy]
+  resources :bookmarks, only: [:destroy]
 end
